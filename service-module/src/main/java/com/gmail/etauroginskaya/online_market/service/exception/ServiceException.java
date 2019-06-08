8f63b5d6a83a6733d1727ac0bb92ea1bf18388cc
etauroginskaya <etauroginskaya@gmail.com>
2019-06-08 09:08:11 +0300

    Resolved merge conflict

--- service-module/src/main/java/com/gmail/etauroginskaya/online_market/service/exception/ServiceException.java
+++ service-module/src/main/java/com/gmail/etauroginskaya/online_market/service/exception/ServiceException.java
@@ -1,15 +1,7 @@
 package com.gmail.etauroginskaya.online_market.service.exception;
 
 public class ServiceException extends RuntimeException {
-<<<<<<< HEAD
-
     public ServiceException(String message, Exception e) {
         super(message, e);
     }
 }
-=======
-    public ServiceException(String message, Exception e) {
-        super(message, e);
-    }
-}
->>>>>>> develop
--- service-module/src/main/java/com/gmail/etauroginskaya/online_market/service/util/CoderUtil.java
+++
@@ -1,6 +0,0 @@
-package com.gmail.etauroginskaya.online_market.service.util;
-
-public interface CoderUtil {
-
-    String encode(String password);
-}
